44c6a38f0a56d836f99ea6b06832818d5aaaeb26
Erlend <49862976+Erb3@users.noreply.github.com>
2023-07-24 19:38:07 +0200

    fix: Incorrect meta url

--- src/meta.js
+++ src/meta.js
@@ -7,7 +7,7 @@
 // @grant       GM_addStyle
 // @downloadURL https://p.sc3.io/api/v1/pastes/TUWRCk8J6t/raw
 // @supportURL  https://github.com/Erb3/SCPasteUtils/issues
-// @homepageURL https://github.com/Erb3/SCPasteUtils/issues
+// @homepageURL https://github.com/Erb3/SCPasteUtils/
 // @version     process.env.VERSION
 // @author      process.env.AUTHOR
 // @require     https://cdn.jsdelivr.net/combine/npm/@violentmonkey/dom@2,npm/@violentmonkey/ui@0.7
